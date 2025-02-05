af17aa2480c5a83344031c42b345f71455d18fbd
SJMiguel8 <bonilla.miguel.e@gmail.com>
2025-02-05 14:56:35 -0600

    I wanna give a shoutout to christina applegate

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import '../styles/search.css';
 
-<div className="search"> 
-    <h1>Search</h1>
-    <p>My search page.</p>
-</div>
-
 export default function Search() {
     return (
         <div className="search">
@@ -15,11 +10,6 @@ export default function Search() {
             <p>
                 My search page.
             </p>
-            <h2>
-                Search results will appear here.
-            </h2>
-
         </div>
-
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import '../app/globals.css';
 import '../app/page.css';
 import '../app/components/search.tsx';
+import Search from '../app/components/search';
+import JobSearch from './components/jobsearch';
 
 export default function Home() {
   return (
     <div className="container"> 
        <h1>Welcome to thy castle</h1>
-
-     <div className="search">
-       <h1>Search</h1>
-       <p>My search page.</p>
-     </div>
+        <Search />
+        <JobSearch />
     </div>
+
   );
 }
+
