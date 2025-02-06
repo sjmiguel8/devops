0f37a59cf26c7ec5be2ed795cc1da0bb5b07300b
SJMiguel8 <bonilla.miguel.e@gmail.com>
2025-02-05 18:31:37 -0600

    they not like us

--- app/fullstack/page.tsx
+++ app/fullstack/page.tsx
@@ -1,97 +1,17 @@
 'use client';
 
-import axios, { AxiosRequestConfig } from 'axios';
-import React, { Key } from 'react';
+import React from 'react';
 import '../styles/jobsearch.css';
 import '../styles/fullstack.css';
 import Navbar from '../components/navbar';
+import FullStackSearchComponent from '../components/fullstackSearch';
 
-interface Job {
-    job_id: Key | null | undefined;
-    job_title: string;
-    job_description: string;
-    job_employer_name: string;
-}
-
-interface JobSearchState {
-    searchResults: Job[];
-    error: string | null;
-}
-
-export default function FullStackPage() {
+export default function Page() {
     return (
         <div className="title">
             <Navbar />
             <h1>FullStack Jobs in Houston:</h1>
-            <FullStackSearch />
+            <FullStackSearchComponent />
         </div>
     );
 }
-
-class FullStackSearchComponent extends React.Component<object, JobSearchState> {
-    constructor(props: object) {
-        super(props);
-        this.state = {
-            searchResults: [],
-            error: null,
-        };
-    }
-
-    async componentDidMount() {
-        const options = {
-            method: 'GET',
-            url: 'https://jsearch.p.rapidapi.com/search',
-            params: {
-                query: 'fullstack developer jobs in houston',
-                page: '1',
-                num_pages: '1',
-                country: 'us',
-                date_posted: 'all'
-            },
-            headers: {
-                'x-rapidapi-key': '5c307ae576mshb6b3574f1f360e1p19ad0bjsnf53687848ffc',
-                'x-rapidapi-host': 'jsearch.p.rapidapi.com'
-            }
-        };
-
-        try {
-            const response = await axios.request(options as AxiosRequestConfig);
-            this.setState({
-                searchResults: response.data.data,
-                error: null,
-            });
-        } catch (error) {
-            this.setState({
-                searchResults: [],
-                error: 'An error occurred while fetching the search results.',
-            });
-            console.error(error);
-        }
-    }
-
-    render() {
-        const { searchResults, error } = this.state;
-
-        if (error) {
-            return <div className="error-message">{error}</div>;
-        }
-        return (
-            <div className="job-listings">
-                <h2>Houston FullStack Job listings</h2>
-                    {searchResults.map((job: Job) => (
-                        <div key={job.job_id} className="job-card">
-                            <div className="job-header"> 
-                            <h3 className="job-title">{job.job_title}</h3>
-                            <div className="job-company">{job.job_employer_name}</div>
-                            </div>
-                            <div className="job-description">{job.job_description}</div>
-                        </div>
-                    ))}
-            </div>
-        );
-    }
-}
-
-const FullStackSearch = FullStackSearchComponent;
-
-export { FullStackSearch };
